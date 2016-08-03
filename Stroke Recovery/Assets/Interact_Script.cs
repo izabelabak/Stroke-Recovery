4f0990a1701941103370f4dc1249b82480be0386
izabelabak <bakbela87@gmail.com>
2016-08-03 21:47:52 +0200

    Doors

---
+++ Stroke Recovery/Assets/Interact_Script.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interact_Script : MonoBehaviour {
+    public float Distance = 5f;
+    
+
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Distance))
+            {
+                if (hit.collider.CompareTag("door"))
+                {
+                    hit.collider.transform.parent.GetComponent<Door_script>().ChangeDoorState();
+                }
+            }
+        }
+	}
+}

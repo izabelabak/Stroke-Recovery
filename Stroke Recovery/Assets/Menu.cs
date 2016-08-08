fc1ac6462da18d156458289033e598b7491c4289
izabelabak <bakbela87@gmail.com>
2016-08-08 21:24:06 +0200

    Menu

---
+++ Stroke Recovery/Assets/Menu.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public Canvas QuitMenu;
+    public Button LevelOneButton;
+    public Button LevelTwoButton;
+    public Button LevelThreeButton;
+    public Button ExitButton;
+
+    void Start ()
+    {
+        QuitMenu = QuitMenu.GetComponent<Canvas>();
+        LevelOneButton = LevelOneButton.GetComponent<Button>();
+        LevelTwoButton = LevelTwoButton.GetComponent<Button>();
+        LevelThreeButton = LevelThreeButton.GetComponent<Button>();
+        ExitButton = ExitButton.GetComponent<Button>();
+        QuitMenu.enabled = false;
+	}
+
+    public void ExitPress()
+    {
+        QuitMenu.enabled = true;
+        LevelOneButton.enabled = false;
+        LevelTwoButton.enabled = false;
+        LevelThreeButton.enabled = false;
+        ExitButton.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        QuitMenu.enabled = false;
+        LevelOneButton.enabled = true;
+        LevelTwoButton.enabled = true;
+        LevelThreeButton.enabled = true;
+        ExitButton.enabled = true;
+    }
+
+    public void StartLevelOne()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void StartLevelTwo()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void StartLevelThree()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Stroke Recovery/Assets/Scripts/Door_script.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door_script : MonoBehaviour {
+    public bool open = false;
+    public float doorOpenAngle = 90f;
+    public float doorCloseAngle = 0f;
+    public float smooth = 2f;
+
+	void Start () {
+	
+	}
+	
+    public void ChangeDoorState()
+    {
+        open = !open;
+    }
+
+	void Update () {
+	    if (open)
+        {
+            Quaternion targetRotation = Quaternion.Euler(0, doorOpenAngle, 0);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
+        }
+        else
+        {
+            Quaternion targetRotation2 = Quaternion.Euler(0, doorCloseAngle, 0);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation2, smooth * Time.deltaTime);
+        }
+	}
+}
